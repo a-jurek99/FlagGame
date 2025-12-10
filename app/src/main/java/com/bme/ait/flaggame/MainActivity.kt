cbbdb2f4945416632129eee193896787bc464526
a-jurek99 <ajurek@pugetsound.edu>
2025-12-10 15:43:29 +0100

    I did have to update gradle... :(

--- app/src/main/java/com/bme/ait/flaggame/MainActivity.kt
+++ app/src/main/java/com/bme/ait/flaggame/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bme.ait.flaggame.ui.screen.menuScreen.MenuScreen
+import com.bme.ait.flaggame.ui.screen.menuScreen.MenuViewModel
 import com.bme.ait.flaggame.ui.theme.FlagGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FlagGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MenuScreen(viewModel = MenuViewModel())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlagGameTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/bme/ait/flaggame/ui/screen/menuScreen/MenuScreen.kt
+++ app/src/main/java/com/bme/ait/flaggame/ui/screen/menuScreen/MenuScreen.kt
@@ -1,4 +1,42 @@
 package com.bme.ait.flaggame.ui.screen.menuScreen
 
-class MenuScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuScreen(
+    viewModel: MenuViewModel
+) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Flag Guessing Game") },
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier  = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ){
+            //logo of some kind?
+            TextButton(
+                onClick = {
+                    //start game
+                }
+            ){
+                Text("Play")
+            }
+            //if high score exists, display
+        }
+    }
 }
